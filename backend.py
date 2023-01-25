ec1b604e067d46420490d6b2062ccf346292e302
Daanish-Khan <dkhan045@uottawa.ca>
2023-01-25 11:02:46 -0500

    initial setup

---
+++ backend.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import pandas as pd
+import ast
+
+app = Flask(__name__)
+api = Api(app)
+
+class Key(Resource):
+    def get(self):
+        args = request.args
+        return {'key': args['k'], 'length': len(args['k'])}, 200
+    pass
+
+api.add_resource(Key, '/key')
+
+if __name__ == '__main__':
+    app.run()
