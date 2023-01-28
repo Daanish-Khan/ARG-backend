09f3cc48b0f438e917eb7e1671168d9644bd2406
Daanish-Khan <dkhan045@uottawa.ca>
2023-01-28 13:45:05 -0500

    event getter

--- backend.py
+++ backend.py
@@ -24,17 +24,41 @@ class Key(Resource):
     def get(self):
         args = request.args
 
-        # Check if key exists
-        doc_ref = db.collection('keys').document(args['k'])
-        doc = doc_ref.get()
+        # Get requested key from database
+        key_ref = db.collection('keys').document(args['k'])
+        key = key_ref.get()
+
+        # Get active event
+        event_ref = db.collection('events').document('CURRENTEVENT')
+        event = event_ref.get()
+
+        # Check if key to event matches active event
+        if event.to_dict()['event'] != key.to_dict()['event']:
+            return {'key': args['k'], 'valid': False}, 200
 
         # Returns key, validity, and what event the key triggers (if valid key)
-        if doc.exists:
-            return {'key': args['k'], 'valid': True, 'trigger': doc.to_dict()['return']}, 200
+        if key.exists:
+
+            # Update current event
+            event_ref.update({u'event': event.to_dict()['event'] + 1})
+
+            return {'key': args['k'], 'valid': True, 'trigger': key.to_dict()['event']}, 200
         else:
              return {'key': args['k'], 'valid': False}, 200
         
     pass
 
+# Event getter
+class Event(Resource):
+
+    def get(self):
+        
+        # Get event
+        event_ref = db.collection('events').document('CURRENTEVENT')
+        event = event_ref.get()
+
+        return {'event': event.to_dict()['event']}, 200
+
 # Add endpoints to api
 api.add_resource(Key, '/key')
+api.add_resource(Key, '/event')
